a0570a7900a2d3972d243fd4667d3aa170841d6b
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2025-03-27 17:21:10 -0400

    update the app.py

--- backend/promotionPackage.py
+++ backend/promotionPackage.py
@@ -11,50 +11,50 @@ class Promotion(db.Model):
     promotion_stripe_id = db.Column(db.String(50), nullable=False)
 
 # Create
-def create_user():
+def create_promotion():
     data = request.json
-    new_user = Promotion(
+    new_promotion = Promotion(
         width=data['width'],
         length=data['length'],
         promotion_stripe_id=data['promotion_stripe_id']
     )
-    db.session.add(new_user)
+    db.session.add(new_promotion)
     db.session.commit()
-    return jsonify({"message": "Promotion created", "id": new_user.id}), 201
+    return jsonify({"message": "Promotion created", "id": new_promotion.id}), 201
 
-# Read (all users)
-def get_all_users():
-    users = Promotion.query.all()
+# Read (all promotions)
+def get_all_promotions():
+    promotions = Promotion.query.all()
     return jsonify([{
-        "id": user.id,
-        "width": user.width,
-        "length": user.length,
-        "promotion_stripe_id": user.promotion_stripe_id
-    } for user in users]), 200
+        "id": promotion.id,
+        "width": promotion.width,
+        "length": promotion.length,
+        "promotion_stripe_id": promotion.promotion_stripe_id
+    } for promotion in promotions]), 200
 
-# Read (single user)
-def get_user(user_id):
-    user = Promotion.query.get_or_404(user_id)
+# Read (single promotion)
+def get_promotion(promotion_id):
+    promotion = Promotion.query.get_or_404(promotion_id)
     return jsonify({
-        "id": user.id,
-        "width": user.width,
-        "length": user.length,
-        "promotion_stripe_id": user.promotion_stripe_id
+        "id": promotion.id,
+        "width": promotion.width,
+        "length": promotion.length,
+        "promotion_stripe_id": promotion.promotion_stripe_id
     }), 200
 
 # Update
-def update_user(user_id):
-    user = Promotion.query.get_or_404(user_id)
+def update_promotion(promotion_id):
+    promotion = Promotion.query.get_or_404(promotion_id)
     data = request.json
-    user.width = data.get('width', user.width)
-    user.length = data.get('length', user.length)
-    user.promotion_stripe_id = data.get('promotion_stripe_id', user.promotion_stripe_id)
+    promotion.width = data.get('width', promotion.width)
+    promotion.length = data.get('length', promotion.length)
+    promotion.promotion_stripe_id = data.get('promotion_stripe_id', promotion.promotion_stripe_id)
     db.session.commit()
     return jsonify({"message": "Promotion updated"}), 200
 
 # Delete
-def delete_user(user_id):
-    user = Promotion.query.get_or_404(user_id)
-    db.session.delete(user)
+def delete_promotion(promotion_id):
+    promotion = Promotion.query.get_or_404(promotion_id)
+    db.session.delete(promotion)
     db.session.commit()
     return jsonify({"message": "Promotion deleted"}), 200
