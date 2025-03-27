7c9db746c36414c5c83c5f5416e7d671f2cfc623
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2025-03-26 20:44:38 -0400

    Integrated Stripe into the project
    Merged MergedFrontBackend into Setting-Up-Stripe

--- frontend/sees-react/src/App.jsx
+++ frontend/sees-react/src/App.jsx
@@ -1,7 +1,9 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./pages/login";
@@ -10,15 +12,23 @@ import Signup from "./pages/signup";
 import CreateEvent from "./pages/createEvent";
 import Dashboard from "./pages/orgDashboard";
 import Event from "./pages/eventPage";
+import Checkout from './components/Checkout';
+import Success from './components/Success';
+import Canceled from './components/Canceled';
 
+import './css/normalize.css';
+import './css/global.css';
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <div className="page-container">
           <Navbar />
           <main className="main-content">
             <Routes>
+              <Route path="/success.html" element={<Success />} />
+              <Route path="/canceled.html" element={<Canceled />} />
+              <Route path="/checkout" element={<Checkout />} />
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
@@ -30,8 +40,8 @@ const App = () => {
           <Footer />
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
-
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
